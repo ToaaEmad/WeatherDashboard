b424700471f449c9f75643dcff5e708f57e6032c
Toaa EmadEldin Muhammad <166917089+ToaaEmad@users.noreply.github.com>
2024-07-06 00:04:19 +0300

    Added Main Files

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function changeView(){
+    let location = document.getElementById("location_input").value;
+    if (location == "ALEXANDRIA"){
+        document.body.style.backgroundImage = "linear-gradient(to right, rgb(23,141,232), rgb(92, 182, 252),rgb(162, 211, 249))";
+    }
+}
